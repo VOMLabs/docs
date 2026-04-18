563672590b918905d63852c81c74732701e83245
ItzzMateo <itzzmateo@devflare.de>
2026-04-18 20:49:42 +0200

    chore: add metadata

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,12 @@
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/lib/layout.shared';
+import { HomeLayout } from "fumadocs-ui/layouts/home";
+import { baseOptions } from "@/lib/layout.shared";
+import type { Metadata } from "next";
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export const metadata: Metadata = {
+  title: "VOMLabs Documentation",
+  description: "The documentation of VOMLabs",
+};
+
+export default function Layout({ children }: LayoutProps<"/">) {
   return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
 }
